490ab87ed784d15c7432aec357af5186546c5daf
HirushaDilshanOfficial <hirushadilshanprofile@gmail.com>
2025-11-20 01:24:26 +0530

    Fix filename case for ProductPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ProductPage from "./Pages/ProductPage.jsx";
-
+import ProductPage from "./pages/productPage.jsx";
 
 import CartPage from "./Pages/CartPage.jsx";
 import { CartProvider } from "./contexts/CartContext";
